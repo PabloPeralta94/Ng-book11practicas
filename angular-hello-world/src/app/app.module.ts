cfd0199e37b479201d79c058440c4cffaea0a658
PabloPeralta94 <pablo.peralta94@gmail.com>
2023-08-24 23:27:28 -0300

    hellow world project

---
+++ angular-hello-world/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserItemComponent } from './user-item/user-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
